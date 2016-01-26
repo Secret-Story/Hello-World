77b7efbec90f9b06326a096f7c467e4340c811d6
yideun <dejun.yi@9tong.com>
2016-01-26 14:23:27 +0800

    hello test

--- src/com/zyt/java/unit1/T.java
+++ src/com/zyt/java/unit1/T.java
@@ -1,8 +1,23 @@
 package com.zyt.java.unit1;
 
+/**
+*
+* @author : Ares.yi
+* @createTime : 2014-11-10 上午11:13:42 
+* @version : 1.0 
+* @description : 
+*
+*/
 public class T {
 
+	/**
+	 * TEST
+	 * @param args
+	 *
+	 * @author : Ares.yi
+	 * @createTime : 2016年1月26日 下午2:22:48
+	 */
 	public static void main(String[] args) {
-		
+		System.out.println("Hello java");
 	}
 }
